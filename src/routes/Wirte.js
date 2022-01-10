6c4f6d36de41bbb6ca4f255b5114d84561610232
Han Byul <yahb9@naver.com>
2022-01-10 15:48:36 +0900

    Feat: Write UI 생성

--- src/routes/Wirte.js
+++ src/routes/Wirte.js
@@ -5,24 +5,96 @@ import styled from 'styled-components'
 
 //서평쓰기 폼 구현
 
-const Writeform = styled.div`
+const Body = styled.div`
 font-family: 'YanoljaYacheR';
 display: flex;
-text-align: center;`
+flex-direction: column;
+text-align: center;
+align-content: center;`
 
 const Title = styled.div`
-display: flex;
+width: 8rem;
+font-size: 28px;
+margin-top: 3.7rem;
+margin-left: 50%;
+transform: translateX(-50%);
 text-align: center;
+align-content: center;
+border-bottom: 2px black solid;
+padding-bottom: 4px;
 `
+
+const Writeform = styled.form`
+margin-left: 50%;
+transform: translateX(-50%);
+margin-top: 3.7rem;
+height: 25rem;
+width: 50rem;
+font-size: 12px;
+`
+
+const Titlebox = styled.input`
+border: none;
+border-top: 2px solid #828282;
+height: 2rem;
+width: 100%;
+:focus {outline:none;};
+font-size: 15px;
+padding-left: 7px;
+`
+
+const Tit = styled.div``
+
+const Infobox = styled.input`
+border: none;
+border-top: 2px solid #828282;
+height: 2rem;
+width: 100%;
+:focus {outline:none;};
+font-size: 15px;
+padding-left: 7px;
+`
+const Info = styled.div``
+
+const Textbox = styled.textarea`
+border: 2px solid #828282;
+height: 18rem;
+width: 100%;
+:focus {outline:none;};
+resize: none;
+font-size: 13px;
+padding-left: 7px;
+padding-top: 7px;
+`
+
+const Subm = styled.button`
+margin-left: 100%;
+transform: translateX(-100%);
+margin-top: 1rem;
+width: 7.5rem;
+height: 2.3rem;
+background: #E8A5A5;
+border: none;
+outline: none;
+font-family: 'YanoljaYacheR' !important;
+font-size: 17px;`
 
 const Write = () => {
     return(
         <>
         <Top />
         <Navigation />
-    <Writeform>
+    <Body>
         <Title>글쓰기</Title>
-    </Writeform>
+        <Writeform>
+            <Titlebox type='text' placeholder='제목'>
+            </Titlebox>
+            <Infobox type='text' placeholder='책 정보'>
+            </Infobox>
+            <Textbox type='text'></Textbox>
+            <Subm>등록</Subm>
+        </Writeform>
+    </Body>
     </>
     )}
 
